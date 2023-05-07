ca7324e33f761cb0647969942667f2917fef2083
Melike Sinem Kaya <msinemkaya6@gmail.com>
2023-05-07 23:08:20 +0300

    feat: filled the inside of the Answer component accordingly

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,7 +1,23 @@
-export default function Answer(){
+export default function Answer({ answerText, index, handleSelectAnswer, currentAnswer, correctAnswer }){
+
+  const letterMapping = ['A', 'B', 'C', 'D']
+  const isCorrect = currentAnswer && answerText === correctAnswer
+  const isWrong = currentAnswer === answerText && currentAnswer !== correctAnswer
+
+  const correctClass = isCorrect ? 'correct-answer' : ''
+  const wrongClass = isWrong ? 'wrong-answer' : ''
+  const disabledClass = currentAnswer ? 'disabled-answer' : ''
+
   return(
     <>
-      
+      <div className={`answer ${correctClass} ${wrongClass} ${disabledClass}`} onClick={() => handleSelectAnswer(answerText)}>
+        <div className='answer-letter'>
+          {letterMapping[index]}
+        </div>
+        <div className='answer-text'>
+          {answerText}
+        </div>
+      </div>
     </>
   );
 }
