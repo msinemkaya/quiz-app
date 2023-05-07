d32637098abd348d0ba14b3e30473e543585157e
Melike Sinem Kaya <msinemkaya6@gmail.com>
2023-05-07 21:29:49 +0300

    feat: added jsx and context hook to Question

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,7 +1,15 @@
+import useQuizContext from "../hooks/quiz-context-hook";
+
 export default function Question(){
+  
+  const [quizState, dispatch] = useQuizContext()
+  const currentQuestion = quizState.questions[quizState.currentQuestionIndex]
+
   return(
     <>
-      
+      <div className="question">
+        {currentQuestion.question}
+      </div>
     </>
   );
 }
