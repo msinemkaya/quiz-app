b6bfd7a4cfeb3433d34465bd05ade9dfbcb09d75
Melike Sinem Kaya <msinemkaya6@gmail.com>
2023-05-07 22:01:24 +0300

    feat: created a next question button

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -6,12 +6,19 @@ export default function Quiz(){
   // quizState is the initialState and/or state as mentioned in quiz.js
   const [quizState, dispatch] = useQuizContext()
 
+  const handleClick = () => {
+    dispatch({
+      type: 'NEXT_QUESTION'
+    })
+  }
+
   return(
     <div className='quiz'>
       <div className='score'>
         Question: {quizState.currentQuestionIndex + 1} / {quizState.questions.length}
       </div>
       <Question />
+      <div className='next-button' onClick={handleClick}>Next Question</div>
     </div>
   );
 }
